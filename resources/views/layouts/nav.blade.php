b617fb920634141c146d68cc1fe2a285b7c7082c
zcroll <hamid2001.nado@gmail.com>
2024-07-04 12:48:33 +0100

    Add navigation layout to order status view
    A new navigation layout file named 'nav.blade.php' has been created. The existing 'order-status.blade.php' view has been updated to extend this new navigation layout, utilizing the 'cc' section for content insertion.

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>App</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+@yield('cc')
+</body>
+</html>
--- resources/views/order-status.blade.php
+++ resources/views/order-status.blade.php
@@ -1,4 +1,5 @@
-
+@extends('layouts.nav')
+@section('cc')
     <ul class="flex items-center justify-between mt-4 text-center">
         @foreach ($statuses as $status => $count)
             @if($status !== 'processing')
@@ -11,3 +12,4 @@
     </ul>
 
 
+@endsection
